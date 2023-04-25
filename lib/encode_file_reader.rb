faba3ac62471be35fe2606b3a36da882c2edd077
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-04-25 14:34:28 -0600

    added encode_file_reader class to read from the CSV file

---
+++ lib/encode_file_reader.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require 'encode'
+
+class EncodesFileReader
+  attr_reader :encodes
+
+  def initialize(file_path)
+    rows = CSV.read(file_path, headers: true, header_converters: :symbol)
+    @encodes = rows.map.with_index(1) do |row, index|
+      encode_data = row.to_hash
+      encode_data[:user_id] = index
+      Encode.new(encode_data)
+    end
+  end
+end
